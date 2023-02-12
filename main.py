be8bba17693c53d9bba9c2df0c9b2b5511ff43fc
YmniyKakYtka <jmaster21042005@mail.ru>
2023-02-12 17:03:17 +0300

    Clear the webdrivers logs in cmd

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup as bs
 
-
 def getHtml(url):
     choise = input('Group or teacher? --> ')
     if choise.lower() == 'group':
@@ -25,7 +24,20 @@ def getHtml(url):
     options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
-    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36')  # Смена user-agent
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-crash-reporter")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-in-process-stack-traces")
+    options.add_argument("--disable-logging")
+    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--log-level=3")
+    options.add_argument("--output=/dev/null")
+    options.add_experimental_option('excludeSwitches', ['enable-logging'])
+    # Смена user-agent
 
     driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     #driver = webdriver.Chrome(f'chromedrivers/chromedriver{version}.exe', options=options)
